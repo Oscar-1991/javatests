df6018c7ff377d3ff51a8c6ac33c0d1337d77d99
Oscar-1991 <oscar1105_@hotmail.com>
2022-07-06 19:09:32 -0500

    Primera parte del proyecto javatests

---
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -0,0 +1,44 @@
+package com.platzi.javatests.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+public class StringUtilTest {
+    @Test
+    public void repeat_string_once() {
+        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+    }
+
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("hola", -1);
+    }
+
+
+
+//    @Test
+//    public void testRepeat() {
+//        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+//        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+//    }
+
+
+//    public static void main(String[] args) {
+//        assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
+//        assertEquals(StringUtil.repeat("hola", 1), "hola");
+//    }
+//    private static void assertEquals(String actual, String expected) {
+//        if (!actual.equals(expected)) {
+//            throw new RuntimeException(actual + " is not equal to expected " + expected);
+//        }
+//    }
+}
